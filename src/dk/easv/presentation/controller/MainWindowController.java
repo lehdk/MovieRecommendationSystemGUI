ec2ccb9372c57077d4328b267c2b7747f8831aae
andersmadsen0511 <anders.bernhard.madsen@gmail.com>
2023-02-14 12:11:04 +0100

    Button actions and removed genre button

--- src/dk/easv/presentation/controller/MainWindowController.java
+++ src/dk/easv/presentation/controller/MainWindowController.java
@@ -1,4 +1,14 @@
 package dk.easv.presentation.controller;
 
+import javafx.event.ActionEvent;
+
 public class MainWindowController {
+    public void handleShowTopMovies(ActionEvent actionEvent) {
+    }
+
+    public void handleShowSimilarUsers(ActionEvent actionEvent) {
+    }
+
+    public void handleShowTopFromSimilar(ActionEvent actionEvent) {
+    }
 }
